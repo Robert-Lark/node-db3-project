a6088b07334707af0a26742f3d8ece9742731c0f
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-10-26 14:57:03 -0700

    completed final sql query

--- queries.sql
+++ queries.sql
@@ -17,3 +17,7 @@ JOIN "Product" ON "OrderDetail"."ProductId" = "Product"."Id"
 WHERE "Order"."Id" = 10251;
 ORDER BY "Product"."ProductName"
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+SELECT "Order"."Id", "Customer"."CompanyName", "Employee"."LastName"
+FROM "Order" 
+JOIN "Customer" ON "Order"."CustomerId" = "Customer"."Id"
+JOIN "Employee" ON "Order"."EmployeeId" = "Employee"."Id"
